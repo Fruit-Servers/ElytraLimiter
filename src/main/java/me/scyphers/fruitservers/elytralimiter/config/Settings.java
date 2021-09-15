004df4f3e258e8d26a6512135ce6fc86cd3e039f
Scyy <bamslizard3201@gmail.com>
2021-09-16 01:47:18 +0800

    Added worlds list to settings and renamed method to reflect changes in the ScyCore library

--- src/main/java/me/scyphers/fruitservers/elytralimiter/config/Settings.java
+++ src/main/java/me/scyphers/fruitservers/elytralimiter/config/Settings.java
@@ -1,25 +1,27 @@
 package me.scyphers.fruitservers.elytralimiter.config;
 
-import me.scyphers.scycore.api.FileSettings;
+import me.scyphers.scycore.api.PluginSettings;
 import me.scyphers.scycore.config.ConfigFile;
 import me.scyphers.scycore.config.FileManager;
 import org.bukkit.configuration.file.YamlConfiguration;
 
-public class Settings extends ConfigFile implements FileSettings {
+import java.util.List;
+
+public class Settings extends ConfigFile implements PluginSettings {
 
     private int saveTicks;
-
     private int maxElytras;
+    private List<String> worlds;
 
     public Settings(FileManager manager) throws Exception {
         super(manager, "config.yml");
     }
 
     @Override
-    public void load(YamlConfiguration configuration, YamlConfiguration defaults) throws Exception {
+    public void load(YamlConfiguration configuration, YamlConfiguration defaults) {
         this.saveTicks = configuration.getInt("fileSaveTicks", 72000);
         this.maxElytras = configuration.getInt("maxElytras", 2);
-
+        this.worlds = configuration.getStringList("worlds");
     }
 
     public int getMaxElytras() {
@@ -29,5 +31,9 @@ public class Settings extends ConfigFile implements FileSettings {
     @Override
     public int getSaveTicks() {
         return saveTicks;
+    }
+
+    public List<String> getWorlds() {
+        return worlds;
     }
 }
