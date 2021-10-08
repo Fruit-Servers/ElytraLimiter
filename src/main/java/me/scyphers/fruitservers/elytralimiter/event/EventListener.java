a011b0ff47873d4ac7e10a8356e3eefcbbff5ca7
Scyy <bamslizard3201@gmail.com>
2021-10-09 01:42:07 +0800

    Rewrite of elytra tracking in attempt to resolve tracking issues

--- src/main/java/me/scyphers/fruitservers/elytralimiter/event/EventListener.java
+++ src/main/java/me/scyphers/fruitservers/elytralimiter/event/EventListener.java
@@ -3,20 +3,19 @@ package me.scyphers.fruitservers.elytralimiter.event;
 import me.scyphers.fruitservers.elytralimiter.ElytraLimiter;
 import me.scyphers.fruitservers.elytralimiter.config.Settings;
 import me.scyphers.scycore.api.Messenger;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.hanging.HangingBreakEvent;
 import org.bukkit.event.hanging.HangingPlaceEvent;
 import org.bukkit.event.inventory.InventoryPickupItemEvent;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 
 import java.util.List;
@@ -33,102 +32,89 @@ public class EventListener implements Listener {
         this.plugin = plugin;
         this.settings = plugin.getSettings();
         this.m = plugin.getMessenger();
-        this.ELYTRA_TRACKING_KEY = new NamespacedKey(plugin, "tagged");
+        this.ELYTRA_TRACKING_KEY = new NamespacedKey(plugin, "elytra-tracking");
         this.PLAYER_PLACED_KEY = new NamespacedKey(plugin, "player-placed");
     }
 
     // TAG MANAGEMENT
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onItemFramePlaceEvent(HangingPlaceEvent event) {
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onItemFrameSpawnEvent(HangingPlaceEvent event) {
 
-        // Only fire if placed by a player
-        if (event.getPlayer() == null) return;
+        // Check if the item frame is in the correct world
+        if (invalidWorld(event.getEntity().getWorld())) return;
 
         // If the entity was not an item frame, ignore the event
         if (event.getEntity().getType() != EntityType.ITEM_FRAME) return;
 
-        // Check if the item frame is in the correct world
-        if (invalidWorld(event.getEntity())) return;
+        // If a player placed this item frame
+        if (event.getPlayer() != null) return;
 
-        // Tag it as being placed by a player
-        event.getEntity().getPersistentDataContainer().set(PLAYER_PLACED_KEY, PersistentDataType.BYTE, (byte) 1);
+        // Apply the tag
+        applyTag(PLAYER_PLACED_KEY, event.getEntity());
+
+        plugin.getLogger().info("Tagged item frame at " + event.getEntity().getLocation() + " with PLAYER_PLACED");
 
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onItemFrameDropItemEvent(HangingBreakEvent event) {
+    public void onItemFrameBreakEvent(HangingBreakEvent event) {
 
-        // If the entity was not an item frame, ignore the event
+        // Only fire in tracked worlds
+        if (invalidWorld(event.getEntity().getWorld())) return;
+
+        // Only track Item Frames
         if (event.getEntity().getType() != EntityType.ITEM_FRAME) return;
-
-        // Check if the item frame is in the correct world
-        if (invalidWorld(event.getEntity())) return;
-
-        // If the item frame was placed by a player, ignore the event
-        if (event.getEntity().getPersistentDataContainer().has(PLAYER_PLACED_KEY, PersistentDataType.BYTE)) return;
 
         ItemFrame frame = (ItemFrame) event.getEntity();
 
-        // If the item frame does not contain an elytra, ignore the event
-        if (frame.getItem().getType() != Material.ELYTRA) return;
+        // Only track item frames with elytras
+        ItemStack item = ((ItemFrame) event.getEntity()).getItem();
+        if (item.getType() != Material.ELYTRA) return;
 
-        // Add the tag to the dropped item
-        frame.getItem().getItemMeta().getPersistentDataContainer().set(ELYTRA_TRACKING_KEY, PersistentDataType.BYTE, (byte) 1);
+        // Check if a player placed this item frame
+        if (frame.getPersistentDataContainer().has(PLAYER_PLACED_KEY, PersistentDataType.BYTE)) return;
+
+        // Update the item in the frame
+        frame.setItem(applyTag(ELYTRA_TRACKING_KEY, item));
+
     }
 
     // ITEM FRAME PROTECTION
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onPlayerInteractItemFrameEvent(PlayerInteractEntityEvent event) {
-
-        // Only watch for item frames
-        if (event.getRightClicked().getType() != EntityType.ITEM_FRAME) return;
-
-        // Only watch in the monitored worlds
-        if (invalidWorld(event.getRightClicked())) return;
-
-        // Check if there is an elytra in the frame
-        ItemFrame frame = (ItemFrame) event.getRightClicked();
-        ItemStack item = frame.getItem();
-        if (item.getType() != Material.ELYTRA) return;
-
-        // This is a new elytra, increment the player count or prevent the pickup attempt
-        // Entity is assumed to be a player at this stage
-        int elytraCount = plugin.getElytraTracker().getElytraAmount(event.getPlayer().getUniqueId());
-        int maxCount = settings.getMaxElytras();
-
-        // The player has reached the max amount of elytras
-        // Prevent the player from picking up this elytra and make this item never despawn
-        // ignore players with the override permission
-        if (elytraCount >= maxCount && !event.getPlayer().hasPermission("elytralimiter.override")) {
-            event.setCancelled(true);
-            m.msg(event.getPlayer(), "elytraMessages.elytraLimitReached", "%max%", Integer.toString(maxCount));
-        }
-
-        // TODO - maybe make the item glow?
-
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onPlayerBreakItemFrameEvent(HangingBreakEvent event) {
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPlayerInteractItemFrameEvent(EntityDamageByEntityEvent event) {
 
         // Only watch for item frames
         if (event.getEntity().getType() != EntityType.ITEM_FRAME) return;
 
         // Only watch in the monitored worlds
-        if (invalidWorld(event.getEntity())) return;
+        if (invalidWorld(event.getEntity().getWorld())) return;
 
         // Check if there is an elytra in the frame
         ItemFrame frame = (ItemFrame) event.getEntity();
         ItemStack item = frame.getItem();
         if (item.getType() != Material.ELYTRA) return;
 
-        // Check for the item tag
-        if (!item.getItemMeta().getPersistentDataContainer().has(ELYTRA_TRACKING_KEY, PersistentDataType.BYTE)) return;
+        // Only fire for players
+        if (!(event.getDamager() instanceof Player player)) return;
 
-        // Prevent breaking the item frame
-        event.setCancelled(true);
+        // Check if the frame is a natural item frame and not placed by the player
+        if (frame.getPersistentDataContainer().has(PLAYER_PLACED_KEY, PersistentDataType.BYTE)) return;
+
+        int elytraCount = plugin.getElytraTracker().getElytraAmount(player.getUniqueId());
+        int maxCount = settings.getMaxElytras();
+
+        // Only cancel the event if the player is over their limit or don't have the override permission
+        if (belowMaxElytras(elytraCount, maxCount, player)) {
+
+            // Cancel the event and inform the player they have hit their max
+            event.setCancelled(true);
+            m.msg(player, "elytraMessages.elytraLimitReached", "%max%", Integer.toString(maxCount));
+
+        }
+
+        plugin.getLogger().info("Player attempted to collect a tagged elytra");
 
     }
 
@@ -145,26 +131,35 @@ public class EventListener implements Listener {
         if (!(event.getEntity() instanceof Player player)) return;
 
         // Only watch in the monitored worlds
-        if (invalidWorld(event.getEntity())) return;
+        if (invalidWorld(event.getEntity().getWorld())) return;
 
         // Check for the item tag
         if (!item.getItemMeta().getPersistentDataContainer().has(ELYTRA_TRACKING_KEY, PersistentDataType.BYTE)) return;
 
-        // This is a new elytra, increment the player count or prevent the pickup attempt
-        // Entity is assumed to be a player at this stage
+        plugin.getLogger().info("Player picked up a tagged elytra");
+
         int elytraCount = plugin.getElytraTracker().getElytraAmount(player.getUniqueId());
         int maxCount = settings.getMaxElytras();
 
-        // The player has reached the max amount of elytras
-        // Prevent the player from picking up this elytra and make this item never despawn
-        // ignore players with the override permission
-        if (elytraCount >= maxCount && !player.hasPermission("elytralimiter.override")) {
+        // Permission and elytra max check
+        if (belowMaxElytras(elytraCount, maxCount, player)) {
+
+            // Player hasn't reached their max elytra count and/or has override
+
+            // add an elytra count for the player
+            plugin.getElytraTracker().addElytra(player.getUniqueId());
+
+            // remove the tracking tag
+            removeTag(ELYTRA_TRACKING_KEY, item);
+
+            // Let the player know
+            m.msg(player, "elytraMessages.acquiredElytra", "%count%", Integer.toString(maxCount - elytraCount), "%max%", Integer.toString(maxCount));
+
+
+        } else {
+            // The player has hit their max, cancel the event and inform them
             event.setCancelled(true);
             m.msg(player, "elytraMessages.elytraLimitReached", "%max%", Integer.toString(maxCount));
-        // The player has not hit the max, don't cancel event
-        } else {
-            plugin.getElytraTracker().addElytra(player.getUniqueId());
-            m.msg(player, "elytraMessages.acquiredElytra", "%count%", Integer.toString(elytraCount), "%max%", Integer.toString(maxCount));
         }
 
     }
@@ -180,7 +175,7 @@ public class EventListener implements Listener {
         if (item.getType() != Material.ELYTRA) return;
 
         // Only watch in the monitored worlds
-        if (invalidWorld(event.getEntity())) return;
+        if (invalidWorld(event.getEntity().getWorld())) return;
 
         // Only watch elytras with the tag
         if (!item.getItemMeta().getPersistentDataContainer().has(ELYTRA_TRACKING_KEY, PersistentDataType.BYTE)) return;
@@ -197,7 +192,7 @@ public class EventListener implements Listener {
     public void onHopperPickupItemEvent(InventoryPickupItemEvent event) {
 
         // Check the world first
-        if (invalidWorld(event.getItem())) return;
+        if (invalidWorld(event.getItem().getWorld())) return;
 
         ItemStack item = event.getItem().getItemStack();
 
@@ -212,10 +207,44 @@ public class EventListener implements Listener {
 
     }
 
-    private boolean invalidWorld(Entity entity) {
+    // UTILITY METHODS
+
+    private boolean invalidWorld(World world) {
         List<String> worlds = settings.getWorlds();
-        World world = entity.getWorld();
         return !worlds.contains(world.getName());
+    }
+
+    private boolean belowMaxElytras(int count, int max, Player player) {
+        return count < max || player.hasPermission("elytralimiter.override");
+    }
+
+    // Tag Management
+
+    /* Temporarily removed to test new method for applying tag
+    private void scheduleApplyTag(Location location, NamespacedKey tag) {
+        plugin.getServer().getScheduler().runTask(plugin, () -> {
+            Collection<Item> items = location.getWorld().getNearbyEntitiesByType(Item.class, location, 2);
+            if (items.size() == 0) return;
+            items.stream().filter(item1 -> item1.getItemStack().getType() == Material.ELYTRA).forEach(item1 -> applyTag(tag, item1.getItemStack()));
+        });
+    }
+     */
+
+    private ItemStack applyTag(NamespacedKey tag, ItemStack item) {
+        ItemMeta itemMeta = item.getItemMeta();
+        itemMeta.getPersistentDataContainer().set(tag, PersistentDataType.BYTE, (byte) 1);
+        item.setItemMeta(itemMeta);
+        return item;
+    }
+
+    private void removeTag(NamespacedKey tag, ItemStack item) {
+        ItemMeta itemMeta = item.getItemMeta();
+        itemMeta.getPersistentDataContainer().remove(tag);
+        item.setItemMeta(itemMeta);
+    }
+
+    private void applyTag(NamespacedKey tag, Entity entity) {
+        entity.getPersistentDataContainer().set(tag, PersistentDataType.BYTE, (byte) 1);
     }
 
 }
